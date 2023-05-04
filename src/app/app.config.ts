dd5cd8a2f4f718cb1a22240fd4d80798fb0d99ab
eptaccio <eptaccio@gmail.com>
2023-05-04 17:34:45 -0300

    chore: remove unused code

--- src/app/app.config.ts
+++
@@ -1,5 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('app', () => ({
-  PORT: process.env.APP_PORT || 3000,
-}));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EligibilityModule } from '../eligibility/eligibility.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     EligibilityModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
